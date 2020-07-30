2071264d1d2ebff9bba76c786f81a97616dc27ff
Lea Otterbeck <Lea@wlan-60-84.wlan.uni-wuppertal.de>
2020-07-30 15:24:05 +0200

    In Metropolis.py some bugs were eliminated. Skipping does work know. Default for init_config is now hot. Number of saved config values was customized. Also there was an allocation problem, because all_configs (which saves the configs) was set equal to the actual config, and in python this means that when actual config changes values the actual config changes the same way. To avoid this, we copy the value from actual config in all_configs. In the change of energy there was a factor of 2 missing, because when we calculate E' - E nothing but one factor changes in all summands, and it changes the way, that we have effectively 2 times this factor (flip from +1 to -1 or the other way around gives us in the substraction a 2). Last we do not save every update step, but we skip some configs.

--- Metropolis.py
+++ Metropolis.py
@@ -2,28 +2,28 @@ import numpy as np
 
 
 class Metropolis:
-    def __init__(self, x_lenght, y_lenght, iterationsteps, beta = 1, interaction = 1):
+    def __init__(self, x_lenght, y_lenght, iterationsteps, beta = 0.8, interaction = 0.3):
         self.NX = x_lenght
         self.NY = y_lenght
         self.total_number_of_points = self.NX * self.NY
         self.itersteps = iterationsteps
-        self.actual_config = self.__init_config()
         self.save_number = 1
-        self.skip = 100
+        self.skip = 1000
         self.inter = interaction
         self.beta = beta
+        self.actual_config = self.__init_config()
 
-    def __init_config(self, categ='cold'):
+    def __init_config(self, categ='hot'):
         """Initialize start configuration hot or cold.
 
         :param categ: value for hot or cold start
         """
-        self.all_configs = [None] * self.itersteps
+        self.all_configs = [None] * (int(self.itersteps/self.skip) + 1)
         if categ == 'cold':
             init = np.ones([self.NX, self.NY]) * 1
         elif categ == 'hot':
             init = np.random.choice([-1, 1], (self.NX, self.NY))
-        self.all_configs[0] = init
+        self.all_configs[0] = np.copy(init)
         return init
 
     def __configurator(self):
@@ -33,10 +33,11 @@ class Metropolis:
         return i, j
 
     def exponential_delta(self, dE):
-        return np.exp(self.beta * dE)
+        return np.exp(-self.beta * dE)
 
     def save(self):
-        self.all_configs[self.save_number] = self.actual_config
+        # copy ist hier sehr sehr wichtig!
+        self.all_configs[self.save_number] = np.copy(self.actual_config)
         self.save_number += 1
 
     def __update_step(self, step):
@@ -45,12 +46,14 @@ class Metropolis:
                      + self.actual_config[nx, ny - 1] \
                      + self.actual_config[(nx + 1) % self.NX, ny] \
                      + self.actual_config[nx, (ny + 1) % self.NY]
-        dE = self.inter * self.actual_config[nx, ny] * neighbours
+        dE = 2 * self.inter * self.actual_config[nx, ny] * neighbours
         r = np.random.random(1)
         if self.exponential_delta(dE) >= r:
-            if step % self.skip == 0:
-                self.save()
             self.actual_config[nx, ny] *= (-1)
+        # nicht jeder Simulationsschritt, ob change nun akzeptiert
+        # oder nicht, soll gespeichert werden
+        if step % self.skip == 0:
+            self.save()
 
     def start_simulation(self):
         for step in range(0, self.itersteps):
